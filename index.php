bb20eac6354ce0f615b26fecd7be009ac8f3489b
David Pavelka <davidpavelka@Davids-MacBook-Pro.local>
2018-08-10 09:26:17 +0200

    next ch

--- index.php
+++ index.php
@@ -5,3 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 $log->addWarning('David Pavelka');
+
+echo 'aaaabbbbb';
+echo 'bbbbbbbbbb';
